9e1c8ea159009a6f19e142a9bbd3025abe855993
skhali <sofia.khali@etu.sorbonne-universite.fr>
2023-04-12 22:33:19 +0200

    removed unecessary files

--- objets/exec_single_builtin.d
+++
@@ -1,8 +0,0 @@
-objets/exec_single_builtin.o: sources/exec_single_builtin.c \
-  includes/list.h libft/includes/libft.h libft/includes/ft_printf.h
-
-includes/list.h:
-
-libft/includes/libft.h:
-
-libft/includes/ft_printf.h:
